0376b12987d4ce30fbf6487a0c37e90e3c99dbad
Dom R <ramutd@outlook.com>
2021-09-22 18:22:29 -0400

    Getter Method 1/2 Setter method

--- RaceWinners/RaceWinners/Models/Group.cs
+++ RaceWinners/RaceWinners/Models/Group.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Reflection.Metadata;
+using System.Threading;
 
 namespace RaceWinners.Models
 {
@@ -9,8 +10,14 @@ namespace RaceWinners.Models
     public class Group
     {
         // Name of group
-        public string Name { get; set; }
-        
+        public string Name
+        {
+            get { return Name;}
+            
+            set {Name = }
+
+        }
+
         /// <summary>
         /// Each spot in the list represents the overall rank of finish place in this group
         /// (not the overall race).  So if index 0 contains a 4, then the first runner in this
