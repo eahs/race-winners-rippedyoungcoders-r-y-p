ce7ef1a683018beacc9712243cb1c3b13cbec597
Gavan <Gavanquimbybusiness@gmail.com>
2021-09-23 17:27:08 -0400

    Group.cs
    Group.cs should not be tampered with unless adding the alg

--- RaceWinners/RaceWinners/Models/Group.cs
+++ RaceWinners/RaceWinners/Models/Group.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Reflection.Metadata;
-using System.Threading;
 
 namespace RaceWinners.Models
 {
@@ -10,25 +8,13 @@ namespace RaceWinners.Models
     public class Group
     {
         // Name of group
-        public string Name
-        {
-            get { return Name;}
-
-            set { Name = ; }
-
-        }
-
+        public string Name { get; set; }
+        
         /// <summary>
         /// Each spot in the list represents the overall rank of finish place in this group
         /// (not the overall race).  So if index 0 contains a 4, then the first runner in this
         /// group placed 4th overall.  
         /// </summary>
-        public List<int> Ranks
-        {
-            get { return Ranks; }
-
-            set { Ranks = ; } //i think the median we calculate is supposed to go here
-
-        }
+        public List<int> Ranks { get; set; }
     }
 }
